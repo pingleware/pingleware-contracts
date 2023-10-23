6ff326ebfa851c882a39654d1a7007943bfd8a74
PIPNGLEWARE <maintainers@pingleware.work>
2023-10-23 19:35:37 -0400

    fixxed TransferAgent library deployment for Shareholder

--- migrations/1697953546_transfer_agent.js
+++ migrations/1697953546_transfer_agent.js
@@ -5,6 +5,7 @@ const Shareholder = artifacts.require("Shareholder")
 
 module.exports = function(_deployer,_network,_accounts) {
   // Use deployer to state migration tasks.
-  // TODO: debug "TransferAgent" -- TransferAgent contains unresolved libraries. You must deploy and link the following libraries before you can deploy a new version of TransferAgent: Shareholder.
-  //_deployer.deploy(Contract,_accounts[9],Transaction.address,ActiveOffering.address)
+  _deployer.deploy(Shareholder)
+  _deployer.link(Shareholder,Contract)
+  _deployer.deploy(Contract,_accounts[9],Transaction.address,ActiveOffering.address)
 };
